d5f26a3958df28658efbc3efd2bf69ad69853ff0
Rishi Raj <rishiraj007bgp@gmail.com>
2023-11-02 13:09:14 +0530

    add buttons

--- src/script.js
+++ src/script.js
@@ -1,1 +1,18 @@
-console.log("Hello");
+
+const homeBtn = document.createElement('button')
+const menuBtn = document.createElement('button')
+const contactBtn = document.createElement('button')
+
+homeBtn.setAttribute('id','header-btn')
+menuBtn.setAttribute('id','content-btn')
+contactBtn.setAttribute('id','footer-btn')
+
+homeBtn.textContent = "HOME"
+menuBtn.textContent = "MENU"
+contactBtn.textContent = "CONTACT"
+
+const btns = [homeBtn,menuBtn,contactBtn]
+
+btns.forEach(btn => {
+    document.body.appendChild(btn)
+})
--- webpack.config.js
+++ webpack.config.js
@@ -18,7 +18,7 @@ module.exports = {
                 type: 'asset/resource',
             },
             {
-                test: /\.(png|svg|jpg|jpeg|gif)$/i,
+                test: /\.(png|svg|jpg|jpeg|gif|avif)$/i,
                 type: 'asset/resource',
             },
         ],
