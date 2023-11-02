91cb35456d5d318ca1b487a821419a4c07fd5bc0
Rishi Raj <rishiraj007bgp@gmail.com>
2023-11-02 13:33:12 +0530

    create header with buttons

--- src/script.js
+++ src/script.js
@@ -1,18 +1,34 @@
+import './style.css'
+
+const body = document.querySelector('body')
+
+const header = document.createElement('div')
+const content = document.createElement('div')
+const footer = document.createElement('div')
 
 const homeBtn = document.createElement('button')
 const menuBtn = document.createElement('button')
 const contactBtn = document.createElement('button')
 
-homeBtn.setAttribute('id','header-btn')
-menuBtn.setAttribute('id','content-btn')
-contactBtn.setAttribute('id','footer-btn')
+header.style.display = 'flex';
+header.style.justifyContent = 'center'
+header.style.alignItems = 'center'
+header.style.gap = '10px';
+header.style.backgroundColor = 'pink';
+header.style.height = '50px'
 
-homeBtn.textContent = "HOME"
-menuBtn.textContent = "MENU"
-contactBtn.textContent = "CONTACT"
+homeBtn.textContent = 'HOME';
+menuBtn.textContent = 'MENU';
+contactBtn.textContent = 'CONTACT';
 
 const btns = [homeBtn,menuBtn,contactBtn]
 
 btns.forEach(btn => {
-    document.body.appendChild(btn)
-})
+    header.appendChild(btn)
+    btn.style.padding = '5px'
+});
+
+body.appendChild(header)
+
+
+
