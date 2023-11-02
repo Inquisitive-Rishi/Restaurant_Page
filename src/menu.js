e596e3ae961a23004bdc625c5fb7c5bd2aa6f6a7
Rishi Raj <rishiraj007bgp@gmail.com>
2023-11-02 16:38:33 +0530

    make 3 sections of food items in menu page

--- src/menu.js
+++ src/menu.js
@@ -2,9 +2,27 @@ import { content } from "./script";
 
 export function showMenuPage() {
     const section = document.createElement('section');
-    const txt = document.createElement('h1')
-    txt.textContent = 'Menu';    
 
-    section.appendChild(txt)
+    const bevrageBox = document.createElement('div')
+    const starterBox = document.createElement('div')
+    const mainCourseBox = document.createElement('div')
+    
+    const boxes = [bevrageBox,starterBox,mainCourseBox] 
+
+    boxes.forEach(box => {
+        box.style.display = 'flex'
+        box.style.borderRadius = '30px'
+        box.style.height = '500px';
+        box.style.width = '800px';    
+        box.style.border = '2px solid brown'
+        box.style.marginBottom = '20px'
+    })
+
+    section.style.display = 'grid'
+    section.style.margin = '30px'
+
+    section.appendChild(bevrageBox)
+    section.appendChild(starterBox)
+    section.appendChild(mainCourseBox)
     content.appendChild(section);
 }
