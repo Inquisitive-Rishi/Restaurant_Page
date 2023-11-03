a07309c7fcab77b2fe6ebf96fb473ec8fbe839e9
Rishi Raj <rishiraj007bgp@gmail.com>
2023-11-03 12:05:42 +0530

    add cards to menu box

--- src/menu.js
+++ src/menu.js
@@ -37,8 +37,7 @@ export function showMenuPage() {
     bevrageBox.style.display = 'flex'
     bevrageBox.style.position = 'relative'
     bevrageBox.style.flexDirection = 'column'
-    bevrageBox.style.flexWrap = 'wrap';
-    bevrageBox.style.padding = '30px'
+    bevrageBox.style.padding = '20px'
     bevrageBox.style.height = '500px';
     bevrageBox.style.width = '800px';    
     bevrageBox.style.border = '2px solid brown'
@@ -46,9 +45,8 @@ export function showMenuPage() {
 
     starterBox.style.display = 'flex'
     starterBox.style.position = 'relative'
-    starterBox.style.flexWrap = 'wrap';
     starterBox.style.flexDirection = 'column'
-    starterBox.style.padding = '30px'
+    starterBox.style.padding = '20px'
     starterBox.style.height = '500px';
     starterBox.style.width = '800px';    
     starterBox.style.border = '2px solid brown'
@@ -56,16 +54,78 @@ export function showMenuPage() {
     
     mainCourseBox.style.display = 'flex'
     mainCourseBox.style.position = 'relative'
-    mainCourseBox.style.flexWrap = 'wrap';
     mainCourseBox.style.flexDirection = 'column'
-    mainCourseBox.style.padding = '30px'
+    mainCourseBox.style.padding = '20px'
     mainCourseBox.style.height = '500px';
     mainCourseBox.style.width = '800px';    
     mainCourseBox.style.border = '2px solid brown'
     mainCourseBox.style.marginBottom = '20px'
     
+    const cardContainer1 = document.createElement('div')
+    const cardContainer2 = document.createElement('div')
+    const cardContainer3 = document.createElement('div')
+    const card1 = document.createElement('div')
+    const card2 = document.createElement('div')
+    const card3 = document.createElement('div')
+    const card4 = document.createElement('div')
+    const card5 = document.createElement('div')
+    const card6 = document.createElement('div')
+    const card7 = document.createElement('div')
+    const card8 = document.createElement('div')
+    const card9 = document.createElement('div')
+    const card10 = document.createElement('div')
+    const card11 = document.createElement('div')
+    const card12 = document.createElement('div')
+    const card13 = document.createElement('div')
+    const card14 = document.createElement('div')
+    const card15 = document.createElement('div')
+    const card16 = document.createElement('div')
+    const card17 = document.createElement('div')
+    const card18 = document.createElement('div')
 
+    bevrageBox.appendChild(cardContainer1)
+    starterBox.appendChild(cardContainer2)
+    mainCourseBox.appendChild(cardContainer3)
+
+    const cardContainers = [cardContainer1,cardContainer2,cardContainer3]
+    const cards = [card1,card2,card3,card4,card5,card6,card7,card8,card9,card10,card11,card12,card13,card14,card15,card16,card17,card18]
+
+    cardContainers.forEach(cc => {
+        cc.style.marginTop = '35px'
+        cc.style.display = 'flex'
+        cc.style.flexWrap = 'wrap';
+        cc.style.justifyContent = 'space-between'
+        cc.style.alignItems = 'space-between'
+        cc.style.aspectRatio = 1;
+    })
+
+    cards.forEach(card => {
+        card.style.height = '45%'
+        card.style.width = '30%'
+        card.style.border = '2px solid black'
+    })
     
+    cardContainer1.appendChild(card1)
+    cardContainer1.appendChild(card2)
+    cardContainer1.appendChild(card3)
+    cardContainer1.appendChild(card4)
+    cardContainer1.appendChild(card5)
+    cardContainer1.appendChild(card6)
+
+    cardContainer2.appendChild(card7)
+    cardContainer2.appendChild(card8)
+    cardContainer2.appendChild(card9)
+    cardContainer2.appendChild(card10)
+    cardContainer2.appendChild(card11)
+    cardContainer2.appendChild(card12)
+
+    cardContainer3.appendChild(card13)
+    cardContainer3.appendChild(card14)
+    cardContainer3.appendChild(card15)
+    cardContainer3.appendChild(card16)
+    cardContainer3.appendChild(card17)
+    cardContainer3.appendChild(card18)
+
 
     section.style.display = 'grid'
     section.style.margin = '30px'
