16f50395792f66a74e46c04ef21ce941f3d27fd8
Rishi Raj <rishiraj007bgp@gmail.com>
2023-11-03 11:20:28 +0530

    replace title with button

--- src/menu.js
+++ src/menu.js
@@ -9,9 +9,9 @@ export function showMenuPage() {
     
     const boxes = [bevrageBox,starterBox,mainCourseBox] 
 
-    const bevTitle = document.createElement('h2');
-    const startTitle = document.createElement('h2');
-    const mainCourseTitle = document.createElement('h2');
+    const bevTitle = document.createElement('button');
+    const startTitle = document.createElement('button');
+    const mainCourseTitle = document.createElement('button');
 
     bevTitle.textContent = 'Bevrages';
     startTitle.textContent = 'Starter';
@@ -23,45 +23,49 @@ export function showMenuPage() {
 
 
     const titles = [bevTitle,startTitle,mainCourseTitle];
-
+    
 
     titles.forEach(title => {
-        title.style.marginLeft = 'auto'
-        title.style.marginRight = 'auto'
-        title.style.marginTop = '10px'    
+        title.style.position = 'absolute'
+        title.style.top = 0;
+        title.style.left = 0;
+        title.style.padding = '10px'
+        title.style.fontWeight = 900;
+        title.style.fontSize = '1.2em'
     })
 
     bevrageBox.style.display = 'flex'
+    bevrageBox.style.position = 'relative'
+    bevrageBox.style.flexDirection = 'column'
     bevrageBox.style.flexWrap = 'wrap';
-    bevrageBox.style.borderRadius = '30px'
+    bevrageBox.style.padding = '30px'
     bevrageBox.style.height = '500px';
     bevrageBox.style.width = '800px';    
     bevrageBox.style.border = '2px solid brown'
     bevrageBox.style.marginBottom = '20px'
 
     starterBox.style.display = 'flex'
+    starterBox.style.position = 'relative'
     starterBox.style.flexWrap = 'wrap';
-    starterBox.style.borderRadius = '30px'
+    starterBox.style.flexDirection = 'column'
+    starterBox.style.padding = '30px'
     starterBox.style.height = '500px';
     starterBox.style.width = '800px';    
     starterBox.style.border = '2px solid brown'
     starterBox.style.marginBottom = '20px'
     
     mainCourseBox.style.display = 'flex'
+    mainCourseBox.style.position = 'relative'
     mainCourseBox.style.flexWrap = 'wrap';
-    mainCourseBox.style.borderRadius = '30px'
+    mainCourseBox.style.flexDirection = 'column'
+    mainCourseBox.style.padding = '30px'
     mainCourseBox.style.height = '500px';
     mainCourseBox.style.width = '800px';    
     mainCourseBox.style.border = '2px solid brown'
     mainCourseBox.style.marginBottom = '20px'
+    
 
-
-    for (let i = 0; i < 6; i++) {
-        const card = document.createElement('div');
-        card.classList.add('card')
-        bevrageBox.appendChild(card)
-    }
-
+    
 
     section.style.display = 'grid'
     section.style.margin = '30px'
