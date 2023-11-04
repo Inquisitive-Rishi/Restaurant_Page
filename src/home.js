577915848dc7bf1e6a305d1c66f2f1d7cb949784
Rishi Raj <rishiraj007bgp@gmail.com>
2023-11-04 15:04:31 +0530

    center everything in homepage

--- src/home.js
+++ src/home.js
@@ -7,11 +7,21 @@ export function showHomePage() {
     content.classList.remove('menu-bg')
     content.classList.remove('wood-bg')
     content.classList.add('home-bg')
+
+    // content.style.alignItems = 'center'
+
     const section = document.createElement('section');
     const title = document.createElement('h1')
     const subtxt = document.createElement('p')
     const timeBtn = document.createElement('button');
     const timeAndLocation = document.createElement('div');
+    const locationTitle = document.createElement('h3')
+    const locationSubtitle = document.createElement('p')
+
+    subtxt.style.fontSize = '1.5em'
+    subtxt.style.fontFamily = 'Times New Roman'
+    subtxt.style.fontWeight = 900;
+
 
     timeBtn.style.border = '2px solid white'
     timeBtn.style.backgroundColor = 'black'
@@ -36,12 +46,19 @@ export function showHomePage() {
     timeBtn.style.padding = '10px';
     timeBtn.style.fontSize = '1.3rem'
 
-    timeBtn.style.marginRight = 'auto';
 
     timeAndLocation.appendChild(timeBtn)
+    timeAndLocation.appendChild(locationTitle)
+    timeAndLocation.appendChild(locationSubtitle)
     section.style.margin = '100px'
 
     header.style.backgroundColor = 'black';
+
+    section.style.display = 'flex'
+    section.style.width = '50%'
+    section.style.textAlign = 'center'
+    section.style.flexDirection = 'column'
+    section.style.alignItems = 'center'
 
     section.appendChild(title)
     section.appendChild(subtxt)
